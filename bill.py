7c9e29eacd947a2670fb837825df7f6d9204a68c
Manjujohn3 <manjujohn301@gmail.com>
2022-11-03 09:41:46 +0530

    commit

---
+++ bill.py
@@ -0,0 +1,27 @@
+while(True):
+    print("select an option:")
+    print("1 tea")
+    print("2 coffee")
+    print("3 burger")
+    print("4 pizza")
+    print("5 shawarma")
+    print("6 generate bill")
+    print("7 exit")
+
+    choice=int(input("enter the choice"))
+
+    if(choice==1):
+       print("tea selected")
+    elif(choice==2):
+        print("coffeee selected")
+    elif(choice==3):
+        print("burger selected")
+    elif(choice==4):
+        print("pizza selected")
+    elif(choice==5):
+        print("shawarma selected")
+    elif(choice==6):
+        print("generate bill")
+    elif(choice==7):
+        break
+    
